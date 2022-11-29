7555f8f9693ebcb89a6aa6080476a6d574782260
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2022-11-29 15:24:28 +0800

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd openwrt
+# Add luci-theme-opentomcat
+svn co https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+# Add luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#Add amlogic管理
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic
+#Add luci-app-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
+#Add luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+#Add luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
