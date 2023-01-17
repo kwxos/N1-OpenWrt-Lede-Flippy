8fec3c5aaffe3e7e15388f6bd823da3d797317b1
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2023-01-17 22:27:55 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,235 +1,42 @@
-#!/bin/bash
-cd openwrt
-cat >> .config <<EOF
 CONFIG_TARGET_armvirt=y
 CONFIG_TARGET_armvirt_64=y
 CONFIG_TARGET_armvirt_64_Default=y
-CONFIG_BTRFS_PROGS_ZSTD=y
-CONFIG_LIBMBEDTLS_HAVE_ARMV8CE_AES=y
-CONFIG_PACKAGE_6in4=y
-CONFIG_PACKAGE_TAR_BZIP2=y
-CONFIG_PACKAGE_TAR_GZIP=y
-CONFIG_PACKAGE_TAR_XZ=y
-CONFIG_PACKAGE_TAR_ZSTD=y
-CONFIG_PACKAGE_attr=y
-CONFIG_PACKAGE_bash=y
-CONFIG_PACKAGE_blkid=y
-CONFIG_PACKAGE_boost=y
-CONFIG_PACKAGE_boost-date_time=y
-CONFIG_PACKAGE_boost-program_options=y
-CONFIG_PACKAGE_boost-system=y
-CONFIG_PACKAGE_brook=y
-CONFIG_PACKAGE_bsdtar=y
-CONFIG_PACKAGE_btrfs-progs=y
-CONFIG_PACKAGE_bzip2=y
-CONFIG_PACKAGE_chattr=y
-CONFIG_PACKAGE_chinadns-ng=y
-CONFIG_PACKAGE_coreutils-nohup=y
-CONFIG_PACKAGE_dosfstools=y
-
-#docker
-CONFIG_PACKAGE_luci-app-docker=m  
-CONFIG_PACKAGE_luci-app-dockerman=y
-CONFIG_DOCKER_KERNEL_OPTIONS=y
-CONFIG_DOCKER_SECCOMP=y
-CONFIG_DOCKER_RES_SHAPE=y
-CONFIG_PACKAGE_docker-compose=y
-CONFIG_PACKAGE_luci-app-dockerman=y
-CONFIG_DOCKER_STO_EXT4=y
-CONFIG_DOCKER_NET_MACVLAN=y
-CONFIG_PACKAGE_docker=y
-CONFIG_PACKAGE_dockerd=y
-CONFIG_PACKAGE_luci-i18n-docker-zh-cn=y
-CONFIG_PACKAGE_luci-i18n-dockerman-zh-cn=y
-CONFIG_PACKAGE_luci-lib-docker=y
-
-
-# CONFIG_PACKAGE_etherwake is not set
-CONFIG_PACKAGE_f2fs-tools=y
-CONFIG_PACKAGE_f2fsck=y
-CONFIG_PACKAGE_fdisk=y
-CONFIG_PACKAGE_gawk=y
-CONFIG_PACKAGE_getopt=y
-CONFIG_PACKAGE_haproxy=y
-CONFIG_PACKAGE_htop=y
-CONFIG_PACKAGE_hysteria=y
-CONFIG_PACKAGE_ip6tables=y
-CONFIG_PACKAGE_ip6tables-mod-nat=y
+CONFIG_TARGET_IMAGES_GZIP=y
+CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y
 CONFIG_PACKAGE_ipv6helper=y
-CONFIG_PACKAGE_kcptun-client=y
-CONFIG_PACKAGE_kcptun-config=y
-CONFIG_PACKAGE_kmod-crypto-crc32c=y
-CONFIG_PACKAGE_kmod-fs-btrfs=y
-CONFIG_PACKAGE_kmod-ipt-nat6=y
-CONFIG_PACKAGE_kmod-iptunnel=y
-CONFIG_PACKAGE_kmod-iptunnel4=y
-CONFIG_PACKAGE_kmod-lib-crc32c=y
-CONFIG_PACKAGE_kmod-lib-raid6=y
-CONFIG_PACKAGE_kmod-lib-xor=y
-CONFIG_PACKAGE_kmod-lib-zlib-deflate=y
-CONFIG_PACKAGE_kmod-lib-zlib-inflate=y
-CONFIG_PACKAGE_kmod-lib-zstd=y
-CONFIG_PACKAGE_kmod-nf-nat6=y
-CONFIG_PACKAGE_kmod-sit=y
-CONFIG_PACKAGE_libarchive=y
-CONFIG_PACKAGE_libatomic=y
-CONFIG_PACKAGE_libattr=y
-CONFIG_PACKAGE_libbz2=y
-CONFIG_PACKAGE_libcap=y
-CONFIG_PACKAGE_libevent2=y
-CONFIG_PACKAGE_libexpat=y
-CONFIG_PACKAGE_libfdisk=y
-CONFIG_PACKAGE_libltdl=y
-CONFIG_PACKAGE_liblua5.3=y
-CONFIG_PACKAGE_liblzma=y
-CONFIG_PACKAGE_liblzo=y
-CONFIG_PACKAGE_libmbedtls=y
-CONFIG_PACKAGE_libminiupnpc=y
-CONFIG_PACKAGE_libmount=y
-CONFIG_PACKAGE_libnatpmp=y
-CONFIG_PACKAGE_libncurses=y
-CONFIG_PACKAGE_libreadline=y
-CONFIG_PACKAGE_libsmartcols=y
-CONFIG_PACKAGE_libstdcpp=y
-CONFIG_PACKAGE_libuv=y
-CONFIG_PACKAGE_libwebsockets-full=y
-CONFIG_PACKAGE_libzstd=y
-CONFIG_PACKAGE_losetup=y
-CONFIG_PACKAGE_lsattr=y
-CONFIG_PACKAGE_lsblk=y
-CONFIG_PACKAGE_luci-app-amlogic=y
-# CONFIG_PACKAGE_luci-app-arpbind is not set
-# CONFIG_PACKAGE_luci-app-ddns is not set
-# CONFIG_PACKAGE_luci-app-nlbwmon is not set
-#梯子
-CONFIG_PACKAGE_luci-app-nps=y
-CONFIG_PACKAGE_luci-app-passwall=y
-CONFIG_PACKAGE_luci-app-passwall2=y
-CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_Brook=y
-CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_Hysteria=y
-CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_IPv6_Nat=y
-CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_NaiveProxy=y
-CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_V2ray=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_IPv6_Nat=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_NaiveProxy=y
-# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server is not set
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_GO=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray_Plugin=y
-# CONFIG_PACKAGE_luci-app-rclone_INCLUDE_rclone-ng is not set
-# CONFIG_PACKAGE_luci-app-rclone_INCLUDE_rclone-webui is not set
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Hysteria=y
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_IPT2Socks=y
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Kcptun=y
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NaiveProxy=y
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Redsocks2=y
-# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_SagerNet_Core is not set
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Libev_Client=y
-# CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_Rust_Server is not set
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Simple_Obfs=y
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Trojan=y
-CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_V2ray_Plugin=y
-CONFIG_PACKAGE_luci-app-ttyd=y
-# CONFIG_PACKAGE_luci-app-unblockmusic_INCLUDE_UnblockNeteaseMusic_Go is not set
-# CONFIG_PACKAGE_luci-app-upnp is not set
-# CONFIG_PACKAGE_luci-app-vsftpd is not set
-# CONFIG_PACKAGE_luci-app-wol is not set
+CONFIG_PACKAGE_kmod-usb3=y
+CONFIG_PACKAGE_kmod-usb-storage=y
+CONFIG_PACKAGE_kmod-usb-storage-extras=y
+CONFIG_PACKAGE_kmod-fs-ext4=y
+CONFIG_PACKAGE_kmod-fs-ntfs=y
+CONFIG_PACKAGE_luci-app-samba=y
+CONFIG_PACKAGE_luci-app-vsftpd=y
 CONFIG_PACKAGE_luci-app-zerotier=y
-CONFIG_PACKAGE_luci-compat=y
-CONFIG_PACKAGE_luci-i18n-amlogic-zh-cn=y
-CONFIG_PACKAGE_luci-i18n-nps-zh-cn=y
-CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y
-CONFIG_PACKAGE_luci-i18n-passwall2-zh-cn=y
-CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y
-#CONFIG_PACKAGE_luci-i18n-unblockmusic-zh-cn=not
-CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=y
-CONFIG_PACKAGE_luci-proto-ipv6=y
-CONFIG_PACKAGE_luci-theme-argon=y
-CONFIG_PACKAGE_luci-theme-opentomcat=y
-# CONFIG_PACKAGE_miniupnpd is not set
-CONFIG_PACKAGE_naiveproxy=y
-CONFIG_PACKAGE_nano=y
-CONFIG_PACKAGE_npc=y
-CONFIG_PACKAGE_odhcp6c=y
-CONFIG_PACKAGE_odhcp6c_ext_cer_id=0
-CONFIG_PACKAGE_odhcpd-ipv6only=y
-CONFIG_PACKAGE_odhcpd_ipv6only_ext_cer_id=0
+CONFIG_PACKAGE_luci-app-ddns=y
+CONFIG_PACKAGE_luci-app-wol=y
+CONFIG_PACKAGE_luci-app-upnp=y
+CONFIG_PACKAGE_luci-app-arpbind=y
+CONFIG_PACKAGE_luci-app-autoreboot=y
+CONFIG_PACKAGE_luci-app-diskman_INCLUDE_btrfs_progs=y
+CONFIG_PACKAGE_luci-app-diskman_INCLUDE_lsblk=y
+CONFIG_PACKAGE_luci-app-filetransfer=y
+CONFIG_PACKAGE_luci-app-nlbwmon=y
+CONFIG_PACKAGE_luci-app-ramfree=y
+CONFIG_PACKAGE_luci-app-rclone_INCLUDE_rclone-webui=y
+CONFIG_PACKAGE_luci-app-rclone_INCLUDE_rclone-ng=y
+CONFIG_PACKAGE_luci-app-ttyd=y
+CONFIG_PACKAGE_luci-app-turboacc=y
+CONFIG_PACKAGE_luci-app-turboacc_INCLUDE_OFFLOADING=y
+CONFIG_PACKAGE_luci-app-vlmcsd=y
+CONFIG_PACKAGE_htop=y
+CONFIG_PACKAGE_automount=y
 CONFIG_PACKAGE_openssh-sftp-server=y
-CONFIG_PACKAGE_parted=y
-CONFIG_PACKAGE_perl=y
-CONFIG_PACKAGE_perl-http-date=y
-CONFIG_PACKAGE_perlbase-base=y
-CONFIG_PACKAGE_perlbase-bytes=y
-CONFIG_PACKAGE_perlbase-charnames=y
-CONFIG_PACKAGE_perlbase-class=y
-CONFIG_PACKAGE_perlbase-config=y
-CONFIG_PACKAGE_perlbase-cwd=y
-CONFIG_PACKAGE_perlbase-dynaloader=y
-CONFIG_PACKAGE_perlbase-errno=y
-CONFIG_PACKAGE_perlbase-essential=y
-CONFIG_PACKAGE_perlbase-fcntl=y
-CONFIG_PACKAGE_perlbase-file=y
-CONFIG_PACKAGE_perlbase-filehandle=y
-CONFIG_PACKAGE_perlbase-getopt=y
-CONFIG_PACKAGE_perlbase-i18n=y
-CONFIG_PACKAGE_perlbase-integer=y
-CONFIG_PACKAGE_perlbase-io=y
-CONFIG_PACKAGE_perlbase-list=y
-CONFIG_PACKAGE_perlbase-locale=y
-CONFIG_PACKAGE_perlbase-params=y
-CONFIG_PACKAGE_perlbase-posix=y
-CONFIG_PACKAGE_perlbase-re=y
-CONFIG_PACKAGE_perlbase-scalar=y
-CONFIG_PACKAGE_perlbase-selectsaver=y
-CONFIG_PACKAGE_perlbase-socket=y
-CONFIG_PACKAGE_perlbase-symbol=y
-CONFIG_PACKAGE_perlbase-tie=y
-CONFIG_PACKAGE_perlbase-time=y
-CONFIG_PACKAGE_perlbase-unicode=y
-CONFIG_PACKAGE_perlbase-unicore=y
-CONFIG_PACKAGE_perlbase-utf8=y
-CONFIG_PACKAGE_perlbase-xsloader=y
-CONFIG_PACKAGE_pv=y
-CONFIG_PACKAGE_redsocks2=y
-CONFIG_PACKAGE_shadowsocks-libev-ss-local=y
-CONFIG_PACKAGE_shadowsocks-libev-ss-redir=y
-# CONFIG_PACKAGE_shadowsocks-rust-ssserver is not set
-CONFIG_PACKAGE_simple-obfs-client=y
-CONFIG_PACKAGE_tar=y
-CONFIG_PACKAGE_terminfo=y
-CONFIG_PACKAGE_trojan=y
-CONFIG_PACKAGE_trojan-go=y
-CONFIG_PACKAGE_trojan-plus=y
-CONFIG_PACKAGE_ttyd=y
-CONFIG_PACKAGE_unzip=y
-CONFIG_PACKAGE_uuidgen=y
-CONFIG_PACKAGE_v2ray-core=y
-CONFIG_PACKAGE_v2ray-geoip=y
-CONFIG_PACKAGE_v2ray-geosite=y
-CONFIG_PACKAGE_v2ray-plugin=y
-# CONFIG_PACKAGE_vsftpd-alt is not set
-# CONFIG_PACKAGE_wol is not set
-CONFIG_PACKAGE_xfs-fsck=y
-CONFIG_PACKAGE_xfs-mkfs=y
-CONFIG_PACKAGE_xray-core=y
-CONFIG_PACKAGE_xray-plugin=y
-CONFIG_PACKAGE_xz=y
-CONFIG_PACKAGE_xz-utils=y
-CONFIG_PACKAGE_zerotier=y
-CONFIG_PERL_NOCOMMENT=y
-# CONFIG_TARGET_ROOTFS_CPIOGZ is not set
-# CONFIG_TARGET_ROOTFS_EXT4FS is not set
-# CONFIG_TARGET_ROOTFS_INITRAMFS is not set
-# CONFIG_TARGET_ROOTFS_SQUASHFS is not set
-CONFIG_ZSTD_OPTIMIZE_O3=y
-CONFIG_boost-compile-visibility-hidden=y
-CONFIG_boost-runtime-shared=y
-CONFIG_boost-static-and-shared-libs=y
-CONFIG_boost-variant-release=y
-CONFIG_PACKAGE_kmod-nf-conntrack-netlink=y
-CONFIG_PACKAGE_nlbwmon=y
-
+CONFIG_PACKAGE_luci-app-adbyby-plus=y
+CONFIG_PACKAGE_luci-app-diskman=y
+CONFIG_PACKAGE_luci-theme-argon=y
+CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Xray=y
+#晶晨宝盒
+CONFIG_PACKAGE_luci-app-amlogic=y
 # WIFI
 CONFIG_PACKAGE_brcmfmac-firmware-43430-sdio=y
 CONFIG_PACKAGE_brcmfmac-firmware-43455-sdio=y
@@ -254,18 +61,14 @@ CONFIG_DRIVER_11AC_SUPPORT=y
 CONFIG_DRIVER_11W_SUPPORT=y
 CONFIG_PACKAGE_iw=y
 # WIFI end
-
-# Remove
-# CONFIG_TARGET_ROOTFS_CPIOGZ is not set
-# CONFIG_TARGET_ROOTFS_INITRAMFS is not set
-# CONFIG_TARGET_ROOTFS_EXT4FS is not set
-# CONFIG_TARGET_ROOTFS_SQUASHFS is not set
-# CONFIG_PACKAGE_kmod-fs-antfs is not set
-# CONFIG_PACKAGE_kmod-fs-ksmbd is not set
-# CONFIG_TARGET_IMAGES_GZIP is not set
-# CONFIG_PACKAGE_luci-app-qos is not set #流量服务质量(QoS)流控
-# CONFIG_v2ray-plugin_INCLUDE_GOPROXY is not set
-# CONFIG_XRAY_CORE_COMPRESS_GOPROXY is not set
-# CONFIG_UnblockNeteaseMusicGo_INCLUDE_GOPROXY is not set
-
-EOF
+#docker
+CONFIG_PACKAGE_luci-app-docker=y
+CONFIG_PACKAGE_docker-compose=y
+CONFIG_PACKAGE_docker-ce=y
+CONFIG_PACKAGE_docker=y
+CONFIG_PACKAGE_dockerd=y
+CONFIG_DOCKER_STO_EXT4=y
+CONFIG_DOCKER_NET_MACVLAN=y
+CONFIG_PACKAGE_luci-app-dockerman=y
+CONFIG_PACKAGE_luci-i18n-docker-zh-cn=y
+CONFIG_PACKAGE_luci-i18n-dockerman-zh-cn=y
