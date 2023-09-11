01275524b6267465651662ada247a887eaaf8589
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2023-09-11 13:48:40 +0800

    Update src.sh

--- src.sh
+++ src.sh
@@ -4,3 +4,4 @@ cd openwrt
 echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
