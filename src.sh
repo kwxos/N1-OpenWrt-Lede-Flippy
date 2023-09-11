497eb0a1f7a6bd62314c090cbc73649ca2bbab5e
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2023-09-11 15:31:54 +0800

    Add files via upload

--- src.sh
+++ src.sh
@@ -4,5 +4,3 @@ cd openwrt
 echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore' >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore-ui' >> feeds.conf.default
