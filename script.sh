e8e262f69e82a1373dd20edb7e8865fdf8297aff
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2023-09-11 14:08:18 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,3 +22,4 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/l
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 #Add luci-app-adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/lean/mentohust
